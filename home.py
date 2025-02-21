6f0e27e2da00ae56cc6aa250e9781b1595cc5c12
ayushman-nayak <ayushman.nayak@tigeranalytics.com>
2025-02-21 11:01:58 +0530

    try

---
+++ home.py
@@ -0,0 +1,14 @@
+print("HI") 
+#---------------
+def f(a):
+    return a*2
+#---------------
+print(f(3))
+print(f(2) + f(3))
+#---------------
+print("help me")
+def f(a):
+    return a*3
+print("BYE")
+print("BYE")
+#---------------
